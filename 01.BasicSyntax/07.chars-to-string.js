a40d54d69540c85fe19374db998199f33352df06
oksanakney <oksanakney@gmail.com>
2024-07-14 15:52:45 +0300

    Update 07.chars-to-string.js

---
+++ 01.BasicSyntax/07.chars-to-string.js
@@ -0,0 +1,9 @@
+function combineAndPrint(char1, char2, char3) {
+    let combinedString = char1 + char2 + char3;
+
+    console.log(`${combinedString}`)
+}
+
+combineAndPrint ('a', 'b', 'c');
+combineAndPrint ('%', '2', 'o');
+combineAndPrint ('1', '5', 'p');
