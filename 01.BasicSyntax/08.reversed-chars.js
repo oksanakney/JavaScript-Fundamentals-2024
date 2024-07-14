01dc6e65bae30b41045178ca1fb3cbfc5b697d31
oksanakney <oksanakney@gmail.com>
2024-07-14 16:22:35 +0300

    Update 08.reversed-chars.js

---
+++ 01.BasicSyntax/08.reversed-chars.js
@@ -0,0 +1,7 @@
+function printReverseChars(char1, char2, char3) {
+    let reversedString = char3 + ' ' + char2 + ' ' + char1
+    console.log(reversedString);
+}
+
+printReverseChars ('A', 'B', 'C');
+printReverseChars ('1', 'L', '&');
