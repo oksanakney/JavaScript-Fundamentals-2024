89ec9b2f8dce30fc9262812f1ae8ad0185748667
oksanakney <oksanakney@gmail.com>
2024-07-14 21:07:42 +0300

    Update 18.modern-times-of-hash.js

---
+++ 01.BasicSyntax/18.modern-times-of-hash.js
@@ -0,0 +1,10 @@
+function findSpecialWords(input) {
+    const regex = /#([A-Za-z]+)/g;
+    let match;
+    let result = [];
+
+    while
+}
+
+findSpecialWords('Nowadays everyone uses # to tag a #special word in #socialMedia');
+findSpecialWords('The symbol # is known #variously in English-speaking #regions as the #number sign');
