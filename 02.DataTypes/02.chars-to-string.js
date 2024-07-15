d912c02efe51b932a3f858ea0c4c9f63ce391f9b
oksanakney <oksanakney@gmail.com>
2024-07-15 18:42:39 +0300

    Update 02.chars-to-string.js

---
+++ 02.DataTypes/02.chars-to-string.js
@@ -0,0 +1,9 @@
+function convertCharsToString(char1, char2, char3) {
+    let combinedString = char1 + char2 + char3;
+    
+    console.log(combinedString);
+}
+
+convertCharsToString('a', 'b', 'c');
+convertCharsToString('%', '2', 'o');
+convertCharsToString('1', '5', 'p');
