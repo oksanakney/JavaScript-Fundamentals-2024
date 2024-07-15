d25e9217f197b34dd0d32fe9df08f667ed697212
oksanakney <oksanakney@gmail.com>
2024-07-15 18:46:42 +0300

    Update 03.town-info.js

---
+++ 02.DataTypes/03.town-info.js
@@ -0,0 +1,8 @@
+function printTownInfo(town, population, area) {
+
+
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+}
+
+printTownInfo('Sofia', 1286383, 492);
+printTownInfo('Plovdiv', 1481353, 512);
