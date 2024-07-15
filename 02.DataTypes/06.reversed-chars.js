0362f13f09ca9377059e8ffc91d7bbfe91e27d23
oksanakney <oksanakney@gmail.com>
2024-07-15 18:56:28 +0300

    Update 06.reversed-chars.js

---
+++ 02.DataTypes/06.reversed-chars.js
@@ -0,0 +1,9 @@
+function reverseChars(char1, char2, char3) {
+    let result = char3 + ' ' + char2 + ' ' + char1;
+    console.log(result);
+}
+
+reverseChars('A', 'B', 'C');
+reverseChars('1',
+'L',
+'&');
