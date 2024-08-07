0923197fc518d3cf41ed85380d648195ad680829
oksanakney <oksanakney@gmail.com>
2024-08-07 16:43:09 +0300

    Update 03.merge-arrays.js

--- 03.Arrays/03.merge-arrays.js
+++ 03.Arrays/03.merge-arrays.js
@@ -1,23 +1,36 @@
-function modifyArrayAndPrint(arr) {
-    // Clone tthe array to avoid modifiing the original one
-    let originalArray = [...arr];
+function mergeArrays(arr1, arr2) {
+    
+    // Initialize the result array
+    let resultArray = [];
+    let maxLength = Math.max(arr1.length, arr2.length);
 
-    let modifiedArray = arr.map(( num, index) => {
-        if (num % 2 === 0) {
-            return num + index;
+    // Iterate the elements of arrays doing the operations between them
+    for (let i = 0; i < maxLength; i++) {
+        let elem1 = arr1[i] || "";
+        let elem2 = arr2[i] || "";
+        if (i % 2 === 0) {
+            let num1 = parseFloat(elem1);
+            let num2 = parseFloat(elem2);
+
+            // If either element is not a number, treat it as 0
+            if (isNaN(num1)) num1 = 0;
+            if (isNaN(num2)) num2 = 0;
+            resultArray.push((Math.floor(num1 + num2)).toString());
+            //resultArray.push((parseInt(arr1[i]) + parseInt(arr2[i])).toString());
         } else {
-            return num - index;
+            resultArray.push(elem1 + elem2);
+            //resultArray.push(arr1[i] + arr2[i]);
         }
-    });
+    }
 
-    let originalSum = originalArray.reduce((acc, val) => num, 0);
-    let modifiedSum = modifiedArray.reduce((acc, val) => num, 0);   
-
+    console.log(resultArray.join(' - '));
    
-
-    console.log("[ " + modifiedArray.join(', ') + " ]ö");
-    console.log(originalSum);
-    console.log(modifiedSum);    
 }
 
-modifyArrayAndPrint([5, 15, 23, 56, 35]);
+mergeArrays(['5', '15', '23', '56', '35'], ['17', '22', '87', '36', '11']);
+mergeArrays(['13', '12312', '5', '77', '4'], ['22', '333', '5', '122', '44']);
+
+
+/*Write a function, which receives two string arrays and merges them into a third array.  
+    • If the index of the element is even, add into the third array the sum of both elements at that index
+    • If the index of the element is odd, add the concatenation of both elements at that index */
